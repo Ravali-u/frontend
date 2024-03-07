ed35d1b31b821ffd308ba52bb2c23a8780ea8cde
Ravali-Uppu <ravaliuppu@lyrostech.com>
2024-03-07 18:34:42 +0530

    modified changes

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import './Navbar.css'
-import logo from '../Assets/logo.png'
-import cart_icon from '../Assets/cart_icon.png'
+// import logo from '/Components/Assets/Assets/logo.png'
+import logo from './logo.svg'
+// import cart_icon from './Assets/cart_icon.png'
 
 export const Navbar = () => {
   return (
