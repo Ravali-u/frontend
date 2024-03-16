2d76a0e59a347add2069060aaa694417c1ca7f00
Ravali-Uppu <ravaliuppu@lyrostech.com>
2024-03-16 11:28:57 +0530

    modified

--- src/Pages/ShopCategory.jsx
+++ src/Pages/ShopCategory.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import './CSS/ShopCategory.css'
+import { ShopContext } from '../Context/ShopContext'
+import { dropdown_icon } from '../Components/Assets/dropdown_icon.png'
+import Item from '../Components/Item/Item'
 
-export default function ShopCategory () {
+
+const ShopCategory= (props) =>{
+  const {all_product} = useContext(ShopContext);
   return (
-    <div>
-        ShopCategory
+    <div className='shop-category'>
+        <img src={props.banner} alt="" />
+        <div className="shopcategory-indexSort">
+          <p>
+            <span>Showing 1-12</span> out of 36 products
+          </p>
+          <div className="shopcategory-sort">
+            Sort by <img src={dropdown_icon} alt=""/>
+
+          </div>
+          <div className="shopcategory-products">
+            {all_product.map((item,i)=>{
+                  if(props.category===item.category){
+                    return <Item/>
+                  }
+            })}
+          </div>
+        </div>
     </div>
   )
 }
+export default ShopCategory
