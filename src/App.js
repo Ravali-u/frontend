183cf840367d3b42d11498672ef07941f8c7aa32
Ravali-Uppu <ravaliuppu@lyrostech.com>
2024-04-19 16:56:37 +0530

    modified

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import ShopCategory from './Pages/ShopCategory'
 import Product from './Pages/Product'
 import Cart from './Pages/Cart'
 import  LoginSignup  from './Pages/LoginSignup';
-// import Footer from './Components/Footer/Footer'
+import Footer from './Components/Footer/Footer'
 import men_banner from './Components/Assets/banner_mens.png'
 import women_banner from './Components/Assets/banner_women.png'
 import kid_banner from './Components/Assets/banner_kids.png'
@@ -27,7 +27,7 @@ function App() {
       <Route path='/cart' element={<Cart/>}/>
       <Route path='/login' element={<LoginSignup/>}/>
       </Routes>
-      {/* <Footer/> */}
+       <Footer/> 
       </BrowserRouter>
 </div>
   );
