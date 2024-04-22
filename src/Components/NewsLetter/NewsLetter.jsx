4e9d382c3481711489ca158e683fc963f2d54a12
Ravali-Uppu <ravaliuppu@lyrostech.com>
2024-04-22 11:06:25 +0530

    update

--- src/Components/NewsLetter/NewsLetter.jsx
+++ src/Components/NewsLetter/NewsLetter.jsx
@@ -7,10 +7,10 @@ const NewsLetter=() =>{
     <div className='newsletter'>
         <h1>Get Exclusive Offers On Your Email</h1>
         <p>Subscribe to our newletter and stay updated</p>
-        </div>
         <div>
         <input type="email" placeholder='Your Email id'/>
         <button>Subscribe</button>
+        </div>
         </div>
         </>
   )
